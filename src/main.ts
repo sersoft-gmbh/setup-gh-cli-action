82e4cae6125d9c3c9bb14fe7a71ed9b3194ff34e
Florian Friedrich <ffried@me.com>
2023-04-05 16:59:50 +0200

    Fix extraction

--- src/main.ts
+++ src/main.ts
@@ -71,17 +71,16 @@ const execName = os.platform().startsWith('win') ? 'gh.exe' : 'gh';
 
 async function setAndCheckOutput(installedVersion: IInstalledVersion) {
     await core.group('Checking installation', async () => {
-        core.addPath(path.join(installedVersion.path, 'bin'));
         if (core.isDebug()) {
             core.debug(`Installed version: ${installedVersion.version}`);
             core.debug(`Installed path: ${installedVersion.path}`);
             core.debug('Contents of path:');
             core.debug(`${fs.readdirSync(installedVersion.path).join('\n')}`);
         }
+        core.addPath(path.join(installedVersion.path, 'bin'));
         const versionOutput = await getExecOutput(execName, ['version']);
-        if (versionOutput.stdout.indexOf(installedVersion.version) < 0) {
+        if (versionOutput.stdout.indexOf(installedVersion.version) < 0)
             throw new Error(`gh version ${installedVersion.version} not found in output: ${versionOutput.stdout}`);
-        }
         core.setOutput('installed-version', installedVersion.version);
     });
 }
@@ -125,8 +124,9 @@ async function install(asset: IReleaseAsset, version: string): Promise<IInstalle
         accept: 'application/octet-stream',
     });
     const extractedPath = await tools.extractTar(downloadedPath);
-    const path = await tools.cacheDir(extractedPath, execName, toolName, version);
-    return {version, path};
+    const subpath = path.join(extractedPath, path.basename(asset.name, path.extname(asset.name)));
+    const cachedPath = await tools.cacheDir(subpath, execName, toolName, version);
+    return {version, path: cachedPath};
 }
 
 function checkCache(version: string): IInstalledVersion | null {
