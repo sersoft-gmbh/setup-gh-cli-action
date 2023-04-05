cfc733e206a6c41bf43665a6e1e3abfae55fcc47
Florian Friedrich <ffried@me.com>
2023-04-05 16:05:52 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# setup-gh-cli-action
-A GitHub action that installs or updates the `gh` CLI
+# Setup GH CLI Action
+
+[![Tests](https://github.com/sersoft-gmbh/setup-gh-cli-action/actions/workflows/tests.yml/badge.svg)](https://github.com/sersoft-gmbh/setup-gh-cli-action/actions/workflows/tests.yml)
+
+A GitHub action that installs or updates the `gh` CLI.
+
+## Inputs
+
+### `version`
+
+The `gh` version to install.<br/>
+Can also be set to `latest` to install the latest available version (including prereleases), or `stable` to install the latest stable release.<br/>
+Defaults to `stable`.
+
+### `github-token`
+
+The token to use for retrieving the release. Can be set to `${{secrets.GITHUB_TOKEN}}`.<br/>
+Use this if you hit rate limits.
+
+## Outputs
+
+### `full-version`
+
+The full version was installed.
+
+
+## Example Usage
+
+```yaml
+uses: sersoft-gmbh/setup-gh-cli-action@v1
+with:
+  version: stable
+```
---
+++ src/main.ts
@@ -0,0 +1,166 @@
+import * as core from '@actions/core';
+import {getExecOutput} from '@actions/exec';
+import * as tools from '@actions/tool-cache';
+import * as github from '@actions/github';
+import * as path from 'path';
+import {Octokit} from '@octokit/rest';
+import {clean as semver_clean, compare as semver_compare} from 'semver';
+import * as os from 'os';
+
+class RequestedVersion {
+    private static isStable(version: string): boolean {
+        return version === 'stable';
+    }
+
+    private static isLatest(version: string): boolean {
+        return version === 'latest';
+    }
+
+    readonly semverVersion: string | null;
+    constructor(public inputVersion: string) {
+        if (RequestedVersion.isStable(inputVersion) || RequestedVersion.isLatest(inputVersion)) {
+            this.semverVersion = null;
+            return;
+        }
+        const v = semver_clean(this.inputVersion);
+        if (!v) throw new Error(`Invalid version: ${this.inputVersion}`);
+        this.semverVersion = v;
+    }
+
+    get isStable(): boolean {
+        return RequestedVersion.isStable(this.inputVersion);
+    }
+
+    get isLatest(): boolean {
+        return RequestedVersion.isLatest(this.inputVersion);
+    }
+
+    get tagName(): string {
+        if (this.isStable || this.isLatest || !this.semverVersion) {
+            throw new Error(`Cannot get tag name for ${this.inputVersion}`);
+        }
+        return `v${this.semverVersion}`;
+    }
+}
+
+interface IInstalledVersion {
+    version: string;
+    path: string;
+}
+
+interface IReleaseAsset {
+    name: string;
+    url: string;
+    browser_download_url: string;
+}
+
+interface IRelease {
+    tag_name: string;
+    assets: IReleaseAsset[];
+}
+
+const toolName = 'gh-cli';
+const execName = 'gh';
+
+async function setAndCheckOutput(installedVersion: IInstalledVersion) {
+    await core.group('Checking installation', async () => {
+        core.addPath(path.dirname(installedVersion.path));
+        const versionOutput = await getExecOutput(execName, ['version']);
+        if (!versionOutput.stdout.indexOf(installedVersion.version)) {
+            throw new Error(`gh version ${installedVersion.version} not found in output: ${versionOutput.stdout}`);
+        }
+        core.setOutput('installed-version', installedVersion.version);
+    });
+}
+
+async function findMatchingRelease(version: RequestedVersion, token: string | null): Promise<IRelease> {
+    let octokit: Octokit;
+    if (token) {
+        octokit = github.getOctokit(token) as any;
+    } else {
+        octokit = new Octokit();
+    }
+
+    const baseParams = {
+        owner: 'cli',
+        repo: 'cli',
+    };
+    if (version.isStable) {
+        const latestRelease = await octokit.repos.getLatestRelease(baseParams);
+        return latestRelease.data;
+    } else if (version.isLatest) {
+        const releasesResp = await octokit.repos.listReleases({
+            ...baseParams,
+            per_page: 100,
+        });
+        const releases = releasesResp.data.filter(r => !!semver_clean(r.tag_name));
+        if (releases.length <= 0)
+            throw new Error('Could not find a valid release!');
+        releases.sort((l, r) => semver_compare(semver_clean(r.tag_name)!, semver_clean(l.tag_name)!));
+        return releases[0];
+    } else {
+        const release = await octokit.repos.getReleaseByTag({
+            ...baseParams,
+            tag: version.tagName,
+        });
+        return release.data;
+    }
+}
+
+async function install(asset: IReleaseAsset, version: string): Promise<IInstalledVersion> {
+    const downloadedPath = await tools.downloadTool(asset.url, undefined, undefined, {
+        accept: 'application/octet-stream',
+    });
+    const extractedPath = await tools.extractTar(downloadedPath);
+    const path = await tools.cacheFile(extractedPath, execName, toolName, version);
+    return {version, path};
+}
+
+function checkCache(version: string): IInstalledVersion | null {
+    const semverVersion = semver_clean(version);
+    if (!semverVersion) throw new Error(`Invalid version: ${version}`);
+    const cachedVersion = tools.find(toolName, semverVersion);
+    if (cachedVersion) {
+        core.info('Found cached version.');
+        return {version: semverVersion, path: cachedVersion};
+    }
+    return null;
+}
+
+async function main() {
+    core.startGroup('Validate input');
+    const version = new RequestedVersion(core.getInput('version', {required: true}));
+    const ghToken = core.getInput('github-token');
+    core.endGroup();
+
+    let installedVersion = await core.group('Checking cache', async () => {
+        if (!version.isStable && !version.isLatest) {
+            return checkCache(version.tagName);
+        }
+        return null;
+    });
+    if (installedVersion) return await setAndCheckOutput(installedVersion);
+
+    let release: IRelease;
+    installedVersion = await core.group('Fetching release', async () => {
+        release = await findMatchingRelease(version, ghToken);
+        return checkCache(release.tag_name);
+    });
+    if (installedVersion) return await setAndCheckOutput(installedVersion);
+
+    installedVersion = await core.group('Installing release', async () => {
+        const version = semver_clean(release.tag_name);
+        if (!version) throw new Error(`Invalid version: ${release.tag_name}`);
+        const assetName = `gh_${version}_${os.platform()}_${os.arch()}.tar.gz`;
+        const asset = release.assets.find(a => a.name === assetName);
+        if (!asset) throw new Error(`Could not find a release asset for '${assetName}'`);
+        return await install(asset, version);
+    });
+    await setAndCheckOutput(installedVersion);
+}
+
+try {
+    main().catch((error) => core.setFailed(error.message));
+} catch (error: any) {
+    core.setFailed(error.message);
+}
