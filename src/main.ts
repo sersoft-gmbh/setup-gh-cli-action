fb0a505dca180ec4fc6e91e217df38adf1b53110
Florian Friedrich <ffried@me.com>
2023-04-05 17:16:09 +0200

    Also drop dot

--- src/main.ts
+++ src/main.ts
@@ -125,7 +125,7 @@ async function install(asset: IReleaseAsset, version: string): Promise<IInstalle
         accept: 'application/octet-stream',
     });
     const extractedPath = await tools.extractTar(downloadedPath);
-    const subPath = path.join(extractedPath, path.basename(asset.name, assetExtension));
+    const subPath = path.join(extractedPath, path.basename(asset.name, `.${assetExtension}`));
     const cachedPath = await tools.cacheDir(subPath, execName, toolName, version);
     return {version, path: cachedPath};
 }
